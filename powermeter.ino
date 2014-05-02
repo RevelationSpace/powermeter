561c4b4b4e038c4b348b591110b10ba75df8b098
Roy Smeding <roy.smeding@gmail.com>
2014-05-02 21:10:26 +0200

    Code now outputs wattage (from time between pulses)

--- powermeter.ino
+++ powermeter.ino
@@ -1,29 +1,42 @@
+// for ULONG_MAX
+#include <limits.h>
+
 // reads out up to 8 S0 outputs (from one I/O port) and dump falling edges to serial.
 
 // constants
-const int NUM_INPUTS		= 3;		// number of inputs
-const int BAUD_RATE		= 115200;	// serial baud rate
-const int DEBOUNCE_INTERVAL	= 30;		// debounce interval in ms
+const int	NUM_INPUTS		= 3;		// number of inputs
+const int	BAUD_RATE		= 115200;	// serial baud rate
+const int	DEBOUNCE_INTERVAL	= 30;		// debounce interval in ms
+const float	PULSE_ENERGY		= 1.8e6;	// energy per pulse in W*ms; 1.8MW*ms = 0.5W*h
 
-// TODO: check
+// the PINx value all S0 inputs are on
 #define INPUT_PIN PINB
 
+
 // global state
+// state for each S0 input
 struct counter {
-	bool outValue;
-	bool lastValue;
-	unsigned long debounceTime;
-} counters[NUM_INPUTS];	// pulse counter for each input
+	// for debouncing
+	bool		outValue;	// debounced value
+	bool		lastValue;	// last actual pin value
+	unsigned long	debounceTime;	// time lastValue has been stable for
+
+	// for tracking time between pulses
+	bool		initialized;	// true when lastTime is valid
+	unsigned long	lastTime;	// last time 
+} counters[NUM_INPUTS];
 
 void setup() {
 	Serial.begin(115200);
-	// inputs are inputs by default -- this is fine
-        unsigned long time = millis();
-        for(int i=0; i<NUM_INPUTS; i++) {
-                counters[i].outValue = true;
-                counters[i].lastValue = true;
-                counters[i].debounceTime = time;
-        }
+	// the relevant pins are inputs by default, so that's fine
+	unsigned long time = millis();
+	for(int i=0; i<NUM_INPUTS; i++) {
+		counters[i].outValue	= true;
+		counters[i].lastValue	= true;
+		counters[i].debounceTime= time;
+		counters[i].initialized	= false;
+		counters[i].lastTime	= 0;
+	}
 }
 
 void loop() {
@@ -39,12 +52,27 @@ void loop() {
 		if((time - counters[i].debounceTime) > DEBOUNCE_INTERVAL) {
 			if(v != counters[i].outValue) {
 				counters[i].outValue = v;
-				if(!counters[i].outValue) {	// falling edge
-		                  Serial.print("Puls ");
-                                  Serial.println(i);
+				if(!counters[i].outValue) {				// falling edge
+					if(counters[i].initialized) {			// not the first falling edge, so we have a reference
+						float power;
+						if(time < counters[i].lastTime) {	// check for overflow since lastTime
+							// we assume it's only overflowed once... more than 50 days between pulses seems a bit silly :)
+							power = PULSE_ENERGY / ((float)ULONG_MAX - ((float)time - (float)counters[i].lastTime));
+						} else {
+							power = PULSE_ENERGY / ((float)time - (float)counters[i].lastTime);
+						}
+
+						Serial.print(i);
+						Serial.print(": ");
+						Serial.print(power);
+						Serial.println("W");
+					}
+
+					counters[i].lastTime	= time;
+					counters[i].initialized	= true;
 				}
 			}
 		}
-                counters[i].lastValue = v;
+		counters[i].lastValue = v;
 	}
 }
